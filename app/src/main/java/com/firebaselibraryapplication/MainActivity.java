9ed630d7011c1a1950df7f5294563106fce83dde
Kavya <kavyah@systemrapid.com>
2018-02-02 12:57:37 +0530

    add ondisconnect method of Firebase.

--- app/src/main/java/com/firebaselibraryapplication/MainActivity.java
+++ app/src/main/java/com/firebaselibraryapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.firebaselibraryapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toggleButton();
+        mFirebaseDatabase.onDisconnect().removeValue(new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError error, DatabaseReference firebase) {
+                if (error != null) {
+                    System.out.println("could not establish onDisconnect event:" + error.getMessage());
+                }
+            }
+        });
     }
 
     private void toggleButton() {
@@ -76,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
+                if (user == null) {
+                    Log.e("no user", "User data is null!");
+                    return;
+                }
                 txtDetails.setText(user.name + ", " + user.email);
                 inputEmail.setText("");
                 inputName.setText("");
